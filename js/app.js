e34f672e5f1de1b31d87d794cb10c871175b7bd2
Shannon Peng <shannonpeng98@gmail.com>
2016-02-07 09:57:37 -0500

    Display data

--- js/app.js
+++ js/app.js
@@ -11,10 +11,30 @@ $(document).on('click', '#competitions li', function(event) {
 	$(event.target).addClass("selected");
 	angular.element($(".main")).scope().setQueryCompetition($(event.target).text().substring(0, $(event.target).text().length));
 	angular.element($(".main")).scope().fetchTeams();
+	angular.element($(".main")).scope().fetchMatches();
+	$(".cueText").fadeOut();
 });
 
 $(document).on('click', '#teams li', function(event) {
-	$("#teams li").removeClass("selected");
-	$(event.target).addClass("selected");
-	angular.element($(".main")).scope().setQueryTeam($(event.target).text().substring(0, $(event.target).text().length));
+	if (!$(event.target).hasClass("selected")) {
+		$("#teams li").removeClass("selected");
+		$(event.target).addClass("selected");
+		angular.element($(".main")).scope().setQueryTeam($(event.target).text().substring(0, $(event.target).text().indexOf(' (')));
+		var tn = $(event.target).text().substring($(event.target).text().indexOf('(') + 1, $(event.target).text().length - 1);
+		angular.element($(".main")).scope().setQueryTeamNumber(tn);
+	}
+	else {
+		$("#teams li").removeClass("selected");
+		angular.element($(".main")).scope().setQueryTeam(null);
+		angular.element($(".main")).scope().setQueryTeamNumber(null);
+	}
+	angular.element($(".main")).scope().fetchMatches();
+});
+
+$("#matchNumber").on('keyup', function(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+    }
+    angular.element($(".main")).scope().setQueryMatch($("#matchNumber").val());
+    angular.element($(".main")).scope().fetchMatches();
 });
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,9 +2,12 @@ app.controller('MainController', ['$scope', function($scope) {
 	
 	$scope.competitions = [];
 	$scope.teams = [];
+	$scope.matchData = [];
 
-	$scope.competition = "";
-	$scope.team = "";
+	$scope.competition;
+	$scope.team;
+	$scope.teamNumber;
+	$scope.match;
 
 	$scope.setQueryCompetition = function(s) {
 		$scope.competition = s;
@@ -13,6 +16,27 @@ app.controller('MainController', ['$scope', function($scope) {
 	$scope.setQueryTeam = function(s) {
 		$scope.team = s;
 	},
+
+	$scope.setQueryTeamNumber = function(s) {
+		$scope.teamNumber = s;
+	},
+
+	$scope.setQueryMatch = function(s) {
+		$scope.match = s;
+	},
+
+	$scope.checkDuplicateMatches = function(array) {
+		if (array.length == 1)
+			return array;
+		array.sort(function(a, b) {
+		    return parseInt(a.attributes.matchNumber) - parseInt(b.attributes.matchNumber);
+		});
+		for(var i = array.length - 2; i >= 0; i--) {
+			if (parseInt(array[i].attributes.matchNumber) == parseInt(array[i + 1].attributes.matchNumber))
+				array.splice(i, 1);
+		}
+		return array;
+	}
 
 	$scope.fetchCompetitions = function() {
 		var Competition = Parse.Object.extend("Competition");
@@ -48,5 +72,24 @@ app.controller('MainController', ['$scope', function($scope) {
 				}
 			});
 		});
+	}
+
+	$scope.fetchMatches = function() {
+		var MatchData = Parse.Object.extend("MatchData");
+		var query = new Parse.Query(MatchData);
+		query.equalTo("competitionName", $scope.competition);
+		if (!(isNaN(parseInt($scope.match)))) {
+			query.equalTo("matchNumber", parseInt($scope.match));
+		}
+		if (!(isNaN(parseInt($scope.teamNumber)))) {
+			query.equalTo("teamNumber", parseInt($scope.teamNumber));
+		}
+		console.log(query);
+		query.find().then(function(results) {
+			$scope.$apply(function() {
+				$scope.matchData = $scope.checkDuplicateMatches(results);
+			});
+		});
+		console.log($scope.matchData);
 	}
 }]);
